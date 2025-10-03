ff003407ceb756f9173173e04e19b9c0ea48614e
Sofia Valencia <sofiavalenciasvs@gmail.com>
2025-10-03 15:09:58 -0500

    Segundo commit

--- src/pages/RadiosPage.jsx
+++ src/pages/RadiosPage.jsx
@@ -1,14 +1,16 @@
 import "../styles/RadiosPage.css";
-import {useEffect, useState } from "react";
+import { useEffect, useState, useMemo } from "react";
 import useFetchRadio from "../hooks/fetchRadio";
+
 
 const BASE_URL = "https://de1.api.radio-browser.info/json/stations/bycountry/";
 const COUNTRIES_URL = "https://de1.api.radio-browser.info/json/countries/";
 
 export default function ListCountry() {
-  const [country, setCountry] = useState("")
+  const [country, setCountry] = useState("Colombia")
   const [countries, setCountries] = useState([]);
   const { data: items, loading, error, fetchApi } = useFetchRadio();
+  const [q, setQ] = useState("")
 
 
   useEffect(() => {
@@ -24,10 +26,15 @@ export default function ListCountry() {
     }
   }, [country, fetchApi]);
 
+  const filtered = useMemo(() => {
+    const k = q.trim().toLowerCase();
+    return k ? items.filter(c => c.name?.toLowerCase().includes(k)) : items;
+  }, [items, q]);
+
   return (
     <div className="countries-container">
 
-      <div className="container"> 
+      <div className="container">
         <label> Pais: </label>
         <select value={country} onChange={(e) => setCountry(e.target.value)}>
           {countries.map((c) => (
@@ -37,13 +44,13 @@ export default function ListCountry() {
           ))}
         </select>
       </div>
-      
-      
-      <div className="container"> 
+
+
+      <div className="container">
         <label> Radio: </label>
-        <input
-          type="text"
-          placeholder="Ingrese su radio de preferencia"/>
+        <input style={{ color: "black" }} value={q}
+          onChange={(e) => setQ(e.target.value)}
+          placeholder="Ingrese su radio de preferencia" />
       </div>
 
       <button className="load-btn" onClick={() => fetchApi(`${BASE_URL}${country}`)}>
@@ -53,13 +60,17 @@ export default function ListCountry() {
       {loading && <p>Cargando...</p>}
 
       <div className="cards-grid">
-        {items.length > 0 ? (
-          items.map((station) => (
+        {filtered.length > 0 ? (
+          filtered.map((station) => (
             <div key={station.stationuuid} className="card">
               <img
-                src={station.favicon || "https://via.placeholder.com/150"}
+                src={station.favicon || "/imagenes/opcion1.jpg"} // Si la radio no cuenta con ícono se muestra opcion1.jpg
                 alt={station.name}
                 className="card-img"
+                onError={(e) => {
+                  e.currentTarget.onerror = null;
+                  e.currentTarget.src = "/imagenes/opcion2.jpg" // Si falla la carga de la imagen pero exste se muestra opcion2.jpg
+                }}
               />
               <div className="card-body">
                 <h2 className="card-title">{station.name}</h2>
@@ -67,14 +78,19 @@ export default function ListCountry() {
                   {station.language} • {station.country}
                 </p>
                 <audio controls src={station.url}>
-                   Error
+                  Error
                 </audio>
               </div>
             </div>
           ))
         ) : (
           !loading && <p>No hay radios disponibles</p>
+
         )}
+        {!loading && !error && filtered.length === 0 && (
+          <p>No hay resultados para "{q}".</p>)
+
+        }
       </div>
     </div>
   );
